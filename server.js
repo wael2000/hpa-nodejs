5619409c1afc0dfccb2db267c8f6b98d47c0fae4
Waeil Eldoamiry <weldoami@redhat.com>
2020-12-16 16:35:04 +0400

    initial commit

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 var http = require('http');
+var appmetrics = require('appmetrics');
+var monitoring = appmetrics.monitor();
+
 var port = 8080;
 var ip = '0.0.0.0';
+/*
+monitoring.on('initialized', function (env) {
+   env = monitoring.getEnvironment();
+   for (var entry in env) {
+       console.log(entry + ':' + env[entry]);
+   };
+});
+
+monitoring.on('cpu', function (cpu) {
+   console.log('[' + new Date(cpu.time) + '] CPU: ' + cpu.process);
+});
+*/
 
 function fibo(n) {
      if (n < 2)
          return 1;
-     else   return fibo(n - 2) + fibo(n - 1);
+     else   
+         return fibo(n - 2) + fibo(n - 1);
  }
 var server = http.createServer(function (req, res) {
+   
    req.on('data', function (data) {
+      
    });
    if (req.url === "/") {
       req.on('end', function () {
-         console.log("Invoked");
-         console.log(req.headers.num);
+         //console.log("Invoked");
+         //console.log(req.headers.num);
          let result = fibo(req.headers.num);
          res.writeHead(200, {'Content-Type': 'text/html'});
          res.write('<html><head><title></title></head>');
