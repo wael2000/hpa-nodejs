24cf480c65830a2f543de0aa12548a7f06f1ad26
Waeil Eldoamiry <weldoami@redhat.com>
2020-12-17 22:09:47 +0400

    async

--- async.js
+++ async.js
@@ -12,15 +12,23 @@ function fibo(n) {
 const server = http.createServer();
 server.on('request', async (req, res) => 
 {
-    const queryObject = url.parse(req.url,true).query;
-    let num = 5;
-    if (queryObject.num)
-        num=queryObject.num;
-    // memory intense
-    let arr = Array(num * 200000).fill("some string");   // num * 200K 
-    const data = await someAsyncFunc(num);
-    res.end(JSON.stringify(data));
-    arr.reverse();
+     if (req.url === "/started") {
+        res.end('Started');
+     } else if (req.url === "/ready") {
+        res.end('ready');
+     } else if (req.url === "/live") {
+        res.end('live');
+     } else {
+        const queryObject = url.parse(req.url,true).query;
+        let num = 5;
+        if (queryObject.num)
+            num=queryObject.num;
+        // memory intense
+        let arr = Array(num * 200000).fill("some string");   // num * 200K 
+        const data = await someAsyncFunc(num);
+        res.end(JSON.stringify(data));
+        arr.reverse();
+     }
 });
 
 function someAsyncFunc(num) {
