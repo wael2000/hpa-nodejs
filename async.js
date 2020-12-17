1d3fe6c9ee8a7b19a9b41bf7684860d34737498f
Waeil Eldoamiry <weldoami@redhat.com>
2020-12-17 22:03:41 +0400

    async

--- async.js
+++ async.js
@@ -1,24 +1,7 @@
-
-
 const http = require('http');
 const url = require('url');
 
-//var appmetrics = require('appmetrics');
-
-/* var monitoring = appmetrics.monitor();
-monitoring.on('initialized', function (env) {
-    env = monitoring.getEnvironment();
-    for (var entry in env) {
-        console.log(entry + ':' + env[entry]);
-    };
- });
- 
- monitoring.on('cpu', function (cpu) {
-    console.log('[' + new Date(cpu.time) + '] CPU: ' + cpu.process + ' CPU-System:' + cpu.system);
- });
-*/
-
-
+// recursive call --> CPU intense
 function fibo(n) {
     if (n < 2)
         return 1;
@@ -26,16 +9,15 @@ function fibo(n) {
         return fibo(n - 2) + fibo(n - 1);
 }
 
-
 const server = http.createServer();
-
 server.on('request', async (req, res) => 
 {
-    let arr = Array(1000000).fill("some string");
     const queryObject = url.parse(req.url,true).query;
     let num = 5;
     if (queryObject.num)
         num=queryObject.num;
+    // memory intense
+    let arr = Array(num * 200000).fill("some string");   // num * 200K 
     const data = await someAsyncFunc(num);
     res.end(JSON.stringify(data));
     arr.reverse();
@@ -54,4 +36,4 @@ server.listen(8080);
 require('appmetrics-dash').monitor({
     appmetrics: require('appmetrics'),
     server: server,
-  });
+});
